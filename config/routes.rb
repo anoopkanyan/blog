42d7e56ca0530eb599856af7b15243199a713a13
anoopkanyan <anoopkanyan@gmail.com>
2017-04-04 23:14:32 +0530

    Setup basic project functioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
  resources :posts
+ resources :projects  
+
  get 'welcome/index'
  root 'welcome#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403201731) do
+ActiveRecord::Schema.define(version: 20170404150102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,5 +38,13 @@ ActiveRecord::Schema.define(version: 20170403201731) do
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
