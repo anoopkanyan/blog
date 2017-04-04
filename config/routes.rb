4d38e1a08c0549201a8fc42365c809793a90ec42
anoopkanyan <anoopkanyan@gmail.com>
2017-04-05 04:14:17 +0530

    remove devise sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
  
   devise_for :users
- resources :posts
- resources :projects  
- resources :contacts, only: [:new, :create]
- get 'welcome/index'
- root 'welcome#index'
+  resources :posts
+  resources :projects  
+  resources :contacts, only: [:new, :create]
+  get 'welcome/index'
+  root 'welcome#index'
+ 
+  get '*path' => redirect('/')
 
 end
