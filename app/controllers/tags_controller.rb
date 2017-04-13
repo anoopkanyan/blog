1d200fa1d367298f86c928bce8f49601964b54e4
anoopkanyan <anoopkanyan@gmail.com>
2017-04-13 19:28:06 +0530

    update home page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,9 +2,5 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   	@posts = @tag.posts.order("created_at desc").paginate(page: params[:page], per_page: 10)
-  end
-
-  
-		
-	
+  end	
 end
