1bee928b40caee68fc96c9eab86efad2d433419b
anoopkanyan <anoopkanyan@gmail.com>
2017-04-25 22:07:02 +0530

    image sizes reduced

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,11 @@ class Post < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
-  has_attached_file :image
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
