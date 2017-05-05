2046922205f30c6ba6b9f45e7ec0d3d340189c6a
anoopkanyan <anoopkanyan@gmail.com>
2017-05-05 23:42:45 +0530

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def show
   	@posts = Post.all.order("created_at desc")
+  	@trending = @posts.sort_by {|post| post.impressionist_count}.reverse!
   	render template: "pages/#{params[:page]}"
   end	
 end
