256b16d8996d98651cc1ced06529fec0d9f4500c
anoopkanyan <anoopkanyan@gmail.com>
2017-05-07 02:27:49 +0530

    update index page with videos

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
 
 def index
-	@posts = Post.all.limit(3).order("created_at desc")
 	@projects = Project.all.limit(3).order("created_at desc")
+	@posts = Post.all.order("created_at desc")
+  	@trending = @posts.sort_by {|post| post.impressionist_count}.reverse!
 end	
 
 end
