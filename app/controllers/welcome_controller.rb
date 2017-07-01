5b88f73db8b95994456a86db4bd54944621551a3
anoopkanyan <anoopkanyan@gmail.com>
2017-07-01 23:17:33 +0530

    limit the number of articles on main_page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
 def index
 	@projects = Project.all.limit(3).order("created_at desc")
-	@posts = Post.all.order("created_at desc")
+	@posts = Post.all.limit(15).order("created_at desc")
   	@trending = @posts.sort_by {|post| post.impressionist_count}.reverse!
 end	
 
