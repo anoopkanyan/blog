9c18f682bed948f9f1eacdb1aca9b8ea3e58e24e
anoopkanyan <anoopkanyan@gmail.com>
2017-12-31 02:27:02 +0530

    rollback

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@ class PostsController < ApplicationController
 
 	def index
 		@posts = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
-		@postsall = Post.all.order("created_at desc")
 
 		respond_to do |format|
           format.html
--- app/views/posts/index.rss.builder
+++ app/views/posts/index.rss.builder
@@ -5,7 +5,7 @@ xml.rss :version => "2.0" do
     xml.description "This is a blog by ChewTab"
     xml.link root_url
 
-    @postsall.each do |post|
+    @posts.each do |post|
       xml.item do
         xml.title post.title
         xml.description post.content
