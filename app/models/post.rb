7f5581deda877775c311989e1026107f80991523
anoopkanyan <anoopkanyan@gmail.com>
2018-02-06 00:52:35 +0530

    stupid

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ActiveRecord::Base
     thumb: '100x100>',
     square: '200x200#',
     main: '300x200>'
-  }, 
+  }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
