052676d96e13ee41dbfd8dd3a3420cd782ed1f34
anoopkanyan <anoopkanyan@gmail.com>
2018-02-06 00:35:11 +0530

    protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
    },
-    s3_protocol: => :https,
+   :s3_protocol => :https,
 }
 
 
